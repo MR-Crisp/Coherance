31d2c02ebdf1b0eb1f32bd45ff324d546f6340e0
MR_Crisp <aka.amin2005@gmail.com>
2023-02-01 11:39:18 +0000

    updates

--- Neuro.py
+++ Neuro.py
@@ -6,3 +6,6 @@ from ibllib.atlas import AllenAtlas
 ID = '1a60a6e1-da99-4d4e-a734-39b1d4544fad'
 # we are interested in the raw ephys data and the spike sorting data
 
+one = ONE()
+ba = AllenAtlas()
+
