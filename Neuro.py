b884786f101e882c666efbf8ad90a6fc46868469
MR_Crisp <aka.amin2005@gmail.com>
2023-02-04 11:57:50 +0000

    trying to fix dependancy issues

--- Neuro.py
+++ Neuro.py
@@ -3,9 +3,13 @@ from brainbox.io.one import SpikeSortingLoader
 from ibllib.atlas import AllenAtlas
 
 
-ID = '1a60a6e1-da99-4d4e-a734-39b1d4544fad'
+PID = '1a60a6e1-da99-4d4e-a734-39b1d4544fad'
 # we are interested in the raw ephys data and the spike sorting data
 
 one = ONE()
 ba = AllenAtlas()
 
+eid, pname = one.pid2eid(PID)
+sl = SpikeSortingLoader(eid=eid, pname=pname, one=one, atlas=ba)
+spikes, clusters, channels = sl.load_spike_sorting()
+clusters = sl.merge_clusters(spikes, clusters, channels)
